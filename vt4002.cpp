a1bf454dfc3a47eb583c304b8e508a4d1f300cc8
ttha <thomas@t2ft.de>
2021-04-25 09:18:38 +0200

    Added license comments

--- vt4002.cpp
+++ vt4002.cpp
@@ -1,3 +1,25 @@
+// ***************************************************************************
+// Simple tool to set and read temperatures for a VT4002 climate chamber
+// ---------------------------------------------------------------------------
+// vt4002.cpp
+// VT4002 serial device abstraction
+// ---------------------------------------------------------------------------
+// Copyright (C) 2021 by t2ft - Thomas Thanner
+// Waldstraße 15, 86399 Bobingen, Germany
+// ---------------------------------------------------------------------------
+// 2021-04-24  tt  Initial version created
+// ---------------------------------------------------------------------------
+// VTSimple is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at
+// your option) any later version.
+// VTSimple is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with VTSimple.  If not, see <https://www.gnu.org/licenses/>.
+// ***************************************************************************
 #include "vt4002.h"
 #include <QSerialPort>
 #include <QDebug>
